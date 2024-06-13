40f4719a779293d28c8213d57c0403e039e65187
Яна Югай <yanayugai6@gmail.com>
2024-06-13 21:26:38 +0300

    Refactoring tests with image upload

--- backend/tests/test_memes.py
+++ backend/tests/test_memes.py
@@ -3,23 +3,25 @@ import json
 import random
 import string
 
-from backend.app.schemas import MemePostModel
-from backend.crud import create_meme
-from backend.models import Meme
-
 
 def random_lower_string() -> str:
     return "".join(random.choices(string.ascii_lowercase, k=32))
 
 
-def create_random_meme(db) -> Meme:
+def create_random_meme(client) -> dict[str, str]:
     text = random_lower_string()
-    meme_in = MemePostModel(text=text)
-    return create_meme(session=db, meme_data=meme_in)
+    data = {"data": json.dumps({"text": text})}
+    with open("carbon felt.jpg", "rb") as image:
+        response = client.post(
+            url="/memes/",
+            data=data,
+            files={"image": image},
+        )
+        response_data = response.json()
+    return response_data
 
 
 def test_get_memes(
-    db,
     client,
 ):
     response = client.get(
@@ -28,27 +30,26 @@ def test_get_memes(
     content = response.json()
     len_memes_before = len(content)
     assert response.status_code == 200
-    create_random_meme(db)
-    create_random_meme(db)
+    for _ in range(5):
+        create_random_meme(client)
     response_after = client.get(
         url="/memes/",
     )
     content_after = response_after.json()
     len_memes_after = len(content_after)
     assert response.status_code == 200
-    assert (len_memes_after - len_memes_before) == 2
+    assert (len_memes_after - len_memes_before) == 5
 
 
 def test_get_meme(
-    db,
     client,
 ) -> None:
-    meme = create_random_meme(db=db)
-    response = client.get(url=f"/memes/{meme.id}/")
+    meme = create_random_meme(client=client)
+    response = client.get(url=f"/memes/{meme['id']}/")
     response_data = response.json()
     assert response.status_code == http.HTTPStatus.OK
-    assert meme.id == response_data["id"]
-    assert meme.text == response_data["text"]
+    assert meme["id"] == response_data["id"]
+    assert meme["text"] == response_data["text"]
 
 
 def test_get_meme_not_found(
@@ -65,21 +66,33 @@ def test_post_meme(
 ) -> None:
     text = random_lower_string()
     data = {"data": json.dumps({"text": text})}
-    response = client.post(url="/memes/", data=data)
-    response_data = response.json()
-    assert response.status_code == http.HTTPStatus.OK
-    assert "id" in response_data
-    assert text == response_data["text"]
+    with open("carbon felt.jpg", "rb") as image:
+        response = client.post(
+            url="/memes/",
+            data=data,
+            files={"image": image},
+        )
+        response_data = response.json()
+        assert response.status_code == http.HTTPStatus.OK
+        assert "id" in response_data
+        assert text == response_data["text"]
+        assert "image" in response_data
 
 
-def test_update_meme(client, db):
-    meme = create_random_meme(db=db)
+def test_update_meme(client):
+    meme = create_random_meme(client=client)
     new_text = random_lower_string()
     data = {"data": json.dumps({"text": new_text})}
-    response = client.put(url=f"/memes/{meme.id}/", data=data)
-    response_data = response.json()
-    assert response.status_code == http.HTTPStatus.OK
-    assert new_text == response_data["text"]
+    with open("fibers.png", "rb") as image:
+        response = client.put(
+            url=f"/memes/{meme['id']}/",
+            data=data,
+            files={'image': image},
+        )
+        response_data = response.json()
+        assert response.status_code == http.HTTPStatus.OK
+        assert new_text == response_data["text"]
+        assert meme["image"] != response_data["image"]
 
 
 def test_update_meme_not_found(
@@ -87,9 +100,13 @@ def test_update_meme_not_found(
 ) -> None:
     new_text = random_lower_string()
     data = {"data": json.dumps({"text": new_text})}
-    response = client.put(url="/memes/999/", data=data)
+    with open("fibers.png", "rb") as image:
+        response = client.put(
+            url="/memes/999/",
+            data=data,
+            files={"image": image},
+        )
     response_data = response.json()
-    print(response_data)
     assert response.status_code == http.HTTPStatus.NOT_FOUND
     assert response_data["detail"] == "Meme not found"
 
@@ -104,9 +121,8 @@ def test_delete_meme_not_found(
 
 
 def test_delete_meme(
-    db,
     client,
 ) -> None:
-    meme = create_random_meme(db=db)
-    response = client.delete(url=f"/memes/{meme.id}/")
+    meme = create_random_meme(client=client)
+    response = client.delete(url=f"/memes/{meme['id']}/")
     assert response.status_code == http.HTTPStatus.NO_CONTENT
